32f74a3e21316f50057983685988e31f0a94e685
Anysia V <anysiavalko@gmail.com>
2019-02-02 17:46:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,4 +15,14 @@ create_project_hash
 #location: project.css("ul.project-meta span.location-name").text
 #project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
-
+def create_project_hash
+  projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+ 
+  # return the projects hash
+  projects
+end
